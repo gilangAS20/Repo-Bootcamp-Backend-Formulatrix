86452f555a7c25b11b805f2d33e73174b6282577
Gilang Agung Saputra <84905621+gilangAS20@users.noreply.github.com>
2023-04-11 17:07:28 +0700

    just trying

---
+++ DAY12_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Text;
+
+class TryFileStream
+{
+    static void Main()
+    {
+        string path = @"E:\bootcamp_formulatrix\Repo-Bootcamp-Backend-Formulatrix\DAY12_3\Testing.txt";
+
+        // pertama cek dulu apakah sudah ada filenya, kalo ada baiknya dihapus
+        if(File.Exists(path))
+        {
+            File.Delete(path);
+        }
+
+        // buat file baru
+        using(FileStream fs = File.Create(path))
+        {
+            AddText(fs, "Gilang mencoba menambahkan text\n");
+            AddText(fs, "Gilang mencoba menambahkan text lagi\n");
+            AddText(fs, "anjyr slebew\n");
+
+            for(int i=1; i < 100; i++)
+            {
+                AddText(fs, Convert.ToString(i));
+            }
+        }
+    }
+    private static void AddText(FileStream fs, string value)
+    {
+        byte[] info = new UTF8Encoding(true).GetBytes(value);
+        fs.Write(info, 0, info.Length);
+    }
+}
