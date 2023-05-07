1cf0c4532c85515b55c76ea9884e0f9f675db81a
Gilang Agung Saputra <84905621+gilangAS20@users.noreply.github.com>
2023-05-07 22:14:55 +0700

    small update

---
+++ Design Pattern - Prototype/Prototype Code/WithPrototype_1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+// Client code
+class Client
+{
+    static void Main(string[] args)
+    {
+        // Create prototype object
+        System.Console.WriteLine("Instance 1:");
+        Car prototypeCar = new Car("Toyota", "Corolla");
+        prototypeCar.PrintInfo();
+
+        // Clone object
+        System.Console.WriteLine("\nInstance 2:");
+        Car newCar = (Car)prototypeCar.Clone();
+        newCar.PrintInfo(); // Output: Brand: Toyota, Model: Corolla
+
+        // Modify cloned object
+        System.Console.WriteLine("\nInstance 3:");
+        newCar.SetBrand("Honda");
+        newCar.SetModel("Civic");
+        newCar.PrintInfo(); // Output: Brand: Honda, Model: Civic
+
+        // make third instance
+        System.Console.WriteLine("\nInstance 4:");
+        Car newCarOne = new Car(prototypeCar.GetBrand(), prototypeCar.GetModel());
+        newCarOne.PrintInfo();
+
+        /*
+        System.Console.WriteLine("\nChane Instance 4 become:");
+        newCarOne.SetBrand("Hyundai");
+        newCarOne.SetBrand("Kona electric");
+        newCarOne.PrintInfo();
+        */
+
+        System.Console.WriteLine("\nEdit Instance 1 and print it: ");
+        prototypeCar.SetBrand("Cherry");
+        prototypeCar.SetModel("Tigo");
+
+        System.Console.WriteLine("Show instance 4 again:");
+        newCarOne.PrintInfo();
+    }
+}
+
+
+// Prototype interface
+public interface IPrototype
+{
+    IPrototype Clone();
+}
+
+// Concrete prototype
+public class Car : IPrototype
+{
+    private string _brand;
+    private string _model;
+
+    public Car(string brand, string model)
+    {
+        _brand = brand;
+        _model = model;
+    }
+
+    // Clone method implementation
+    public IPrototype Clone()
+    {
+        return new Car(_brand, _model);
+    }
+
+    public void SetBrand(string brand)
+    {
+        _brand = brand;
+    }
+
+    public void SetModel(string model)
+    {
+        _model = model;
+    }
+
+    public string GetBrand()
+    {
+        return _brand;
+    }
+
+    public string GetModel()
+    {
+        return _model;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Brand: {_brand}, Model: {_model}");
+    }
+}
---
+++ Learn C# .NET from Microsoft/1_LearnFromMicrosoft/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+Console.WriteLine("Hello, World!");
+
+static void HumanizeQuantities()
+{
+    System.Console.WriteLine("I have activity".ToQuantity(7));
+    System.Console.WriteLine("car".ToQuantity(7));
+    System.Console.WriteLine("goose".ToQuantity(7));
+}
+
+
+
+static void HumanizeDate()
+{
+    System.Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    System.Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    System.Console.WriteLine(DateTime.UtcNow.AddHours(+1).Humanize());
+
+    System.Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    System.Console.WriteLine(TimeSpan.FromDays(7).Humanize());
+    System.Console.WriteLine(TimeSpan.FromDays(14).Humanize());
+    System.Console.WriteLine(TimeSpan.FromHours(5).Humanize());
+}
+
+
+System.Console.WriteLine("Quantities: ");
+HumanizeQuantities();
+
+System.Console.WriteLine("\n");
+HumanizeDate();
